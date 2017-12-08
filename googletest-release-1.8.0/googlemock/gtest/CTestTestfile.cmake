42f16cf0b77d6c1e7eac50383d0512b9504efce1
Tanmay Bangalore <tanmay.bangalore@gm.com>
2017-12-08 11:07:44 -0500

    Use submodules for GoogleTest

--- googletest-release-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fzff9p/tushar/travis_gcov_example/googletest-release-1.8.0/googletest
-# Build directory: /home/fzff9p/tushar/travis_gcov_example/googletest-release-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
